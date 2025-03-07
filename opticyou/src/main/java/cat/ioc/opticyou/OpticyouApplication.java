d32b288b4369d699f223674bb9897ca049757ff8
Diffran <148889182+Diffran@users.noreply.github.com>
2025-03-07 21:09:56 +0100

    first server commit

---
+++ opticyou/src/main/java/cat/ioc/opticyou/OpticyouApplication.java
@@ -0,0 +1,13 @@
+package cat.ioc.opticyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticyouApplication.class, args);
+	}
+
+}
---
+++ opticyou/src/test/java/cat/ioc/opticyou/OpticyouApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.ioc.opticyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpticyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
