9f09cde79972ebb3828c1e461c9f17422dc4c150
SelenaGL <aneles.gomez@gmail.com>
2025-03-09 21:08:04 +0100

    Add files via upload

---
+++ OpticYou/app/src/main/java/com/example/opticyou/data/DataSource.kt
@@ -0,0 +1,12 @@
+
+package com.example.opticyou.data
+
+import com.example.opticyou.Screens
+
+object DataSource {
+    val options=mapOf(
+        "Historial clínic" to Screens.Query.name,
+        "Demanar cita" to Screens.List.name,
+        "Veure cites" to Screens.Add.name,
+        "Pautes" to "4")
+}
---
+++ OpticYou/app/src/main/java/com/example/opticyou/data/LoginResponse.kt
@@ -0,0 +1,9 @@
+package com.example.opticyou.data
+
+/**
+ * Data class to save the server response (login success and role)
+ */
+data class LoginResponse(
+    val success: Boolean,
+    val role: String
+)
