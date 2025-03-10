27deaef12156e3f5fe078c8d4e528571fc286e3f
Diffran <148889182+Diffran@users.noreply.github.com>
2025-03-10 21:54:07 +0100

    wip - jwt service

---
+++ opticyou/src/main/java/cat/ioc/opticyou/service/impl/JwtServiceImpl.java
@@ -0,0 +1,81 @@
+package cat.ioc.opticyou.service.impl;
+
+import cat.ioc.opticyou.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.function.Function;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+    @Value("${app.jwtSecret}")
+    private String secretKey;
+
+    @Override
+    public String getToken(UserDetails userDetails) {
+        return getToken(new HashMap<>(), userDetails);
+    }
+
+    private String getToken(Map<String,Object> extractClaims, UserDetails userDetails){
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Override
+    public String getEmailFromToken(String token) {
+        return getClaim(token,Claims::getSubject);
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = getEmailFromToken(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String getUsernameFromToken(String token){
+        return getClaim(token,Claims::getSubject);
+    }
+    private Claims getAllClaims(String token){
+        return
+                Jwts.parser()
+                        .setSigningKey(getKey())
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token){
+        return getClaim(token, Claims::getExpiration);
+    }
+    private boolean isTokenExpired(String token){
+        return getExpiration(token).before(new Date());
+    }
+}
